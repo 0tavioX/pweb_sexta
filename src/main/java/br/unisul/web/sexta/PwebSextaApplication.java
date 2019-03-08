e3b9ace0b789fb5874096bf35bec6efd79c82924
0tavioX <oivato.otavio@gmail.com>
2019-03-08 20:16:25 -0300

    Projeto criado

---
+++ src/main/java/br/unisul/web/sexta/PwebSextaApplication.java
@@ -0,0 +1,13 @@
+package br.unisul.web.sexta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwebSextaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwebSextaApplication.class, args);
+	}
+
+}
